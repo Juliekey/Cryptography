b3a893fbf7e0e8dc69ebd4e805239e4af2fa977c
humminggoo <sashap1997@gmail.com>
2017-10-08 18:30:09 +0300

    fix for route and simple shift

--- src/main/java/ciphers/pr2/RouteShift.java
+++ src/main/java/ciphers/pr2/RouteShift.java
@@ -4,17 +4,18 @@ import entities.Cipher;
 import javafx.util.Pair;
 
 public class RouteShift extends Cipher {
-    int n = 4;
     int m = 5;
-    char[][] table = new char[n][m];
+    char[][] table;
 
     public RouteShift() {
 
         name = "Route Shift";
-        key = n+", "+m;
+        key = m+"";
     }
 
     private  void add(StringBuffer sb){
+        int n = (int)Math.ceil((double) sb.length()/(double)m);
+        table = new char[n][m];
         for (int i = 0; i < table.length; i++){
             for (int j = 0; j < table[i].length; j++){
                 if(sb.length()==0)
@@ -26,14 +27,26 @@ public class RouteShift extends Cipher {
             }
         }
     }
+    private  String modify(StringBuffer sb){
+        for (int i = 0; i < table.length; i++){
+            for (int j = 0; j < table[0].length; j++){
+                sb.append(table[i][j]);
+            }
+        }
+        return sb.toString();
+    }
     public String encrypt(String word) {
         StringBuffer sb = new StringBuffer(word);
         add(sb);
+        String modified = modify(sb);
+        sb.delete(0, sb.length());
         for (int i = 0; i < table[0].length; i++){
             for (int j = 0; j < table.length; j++){
                 sb.append(table[j][i]);
-                tableOfChange.add(new Pair<String, String>(String.valueOf(table[i][j]), String.valueOf(table[j][i])));
             }
+        }
+        for (int i = 0; i < modified.length(); i++){
+                tableOfChange.add(new Pair<String, String>(String.valueOf(modified.charAt(i)), String.valueOf(sb.charAt(i))));
         }
         return sb.toString();
     }
--- src/main/java/ciphers/pr2/SimpleShift.java
+++ src/main/java/ciphers/pr2/SimpleShift.java
@@ -7,8 +7,8 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class SimpleShift extends Cipher {
-    private int[] order = {5, 2, 1, 3, 0, 4};
-    Random rnd;
+    private int[] order;
+    Random rnd = new Random();
 
     public SimpleShift() {
 
@@ -16,7 +16,7 @@ public class SimpleShift extends Cipher {
         key = Arrays.toString(order);
     }
 
-    /*
+
         private void add(int size) {
             order = new int[size];
             for (int i = 0; i < order.length; i++){
@@ -32,12 +32,14 @@ public class SimpleShift extends Cipher {
                 order[i] = a;
             }
         }
-        */
+
     public String encrypt(String word) {
+        add(word.length());
+        shuffle();
         char[] result = new char[word.length()];
         for (int i = 0; i < word.length(); i++){
             result[order[i]] = word.charAt(i);
-            tableOfChange.add(new Pair<String, String>(String.valueOf(word.charAt(order[i])), String.valueOf(word.charAt(i))));
+            tableOfChange.add(new Pair<String, String>(String.valueOf(word.charAt(i)), String.valueOf(word.charAt(order[i]))));
         }
 
         return String.valueOf(result);
