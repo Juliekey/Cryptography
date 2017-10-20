e99fae9f25d1700d9be29855d20b6b71e8645486
Juliekey <yuliya.pedash@gmail.com>
2017-10-20 21:20:33 +0300

    adding RSA encrypt and decrypt methods

--- src/main/java/application/services/CiphersService.java
+++ src/main/java/application/services/CiphersService.java
@@ -5,6 +5,7 @@ import ciphers.pr2.*;
 import ciphers.pr3.GammaModule2;
 import ciphers.pr3.GammaModuleN;
 import ciphers.pr4.ADFGX;
+import ciphers.pr4.RSA;
 import entities.Cipher;
 import entities.Lab;
 import org.springframework.stereotype.Service;
@@ -51,6 +52,9 @@ public class CiphersService {
         ciphers = new HashMap<>();
         ciphers.put(24, new ADFGX());
         allLabs.put(4, new Lab("Lab4", Collections.unmodifiableMap(ciphers)));
+        ciphers = new HashMap<>();
+        ciphers.put(25, new RSA());
+        allLabs.put(5, new Lab("Lab5", Collections.unmodifiableMap(ciphers)));
         return allLabs;
     }
 
